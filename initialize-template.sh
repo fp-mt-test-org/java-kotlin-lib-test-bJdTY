207d6e28e5dbdf347ee061bf6aa0dacd79ce2d82
Matthew Thomas <matthewthomas@flexport.com>
2021-06-12 10:34:31 -0700

    sleep

--- initialize-template.sh
+++ initialize-template.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-git config --global user.name "CI"
-git config --global user.email "ci@ci.com"
+#git config --global user.name "CI"
+#git config --global user.email "ci@ci.com"
 
 git_json="{\"git\":{\"owner\":\"${owner_name}\",\"name\":\"${project_name}\"}}"
 echo "GIT JSON:"
@@ -45,7 +45,7 @@ echo
 
     # destination [default]:
     echo "${git_json}"
+    sleep 1;
 } | battenberg install "${github_base_url}/${template_name}" || true
 
 cat .cookiecutter.json
-
